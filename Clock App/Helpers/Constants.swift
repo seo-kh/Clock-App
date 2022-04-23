246dbc419d3a0a6be356e247c878cea5ce2f5f04
seo-kh <gospelseokh@gmail.com>
2022-04-23 12:47:47 +0900

    Fix the Frame layout and Add the App Icon, Color Set

--- Clock App/Helpers/Constants.swift
+++ Clock App/Helpers/Constants.swift
@@ -9,14 +9,38 @@ import SwiftUI
 
 // MARK: - LAYOUT CONSTATNS
 
+/// padding 레이아웃 값
 let horizontalPadding: CGFloat = 16.0
 let verticalPadding: CGFloat = 8.0
+
+
+/// frame 레이아웃 값
+let screenHeight = UIScreen.main.bounds.height
+let screenWidth = UIScreen.main.bounds.width
+
+let headerHeightRatio = (0.4, 0.5)
+let centerHeightRatio = (0.2, 0.3)
+let footerHeightRatio = (0.1, 0.2)
 
 struct FrameConstant {
     var min: CGSize
     var max: CGSize
 }
 
-let headerFrame = FrameConstant(min: .init(width: 0, height: 300), max: .init(width: 0, height: 400))
-let centerFrame = FrameConstant(min: .init(width: 0, height: 200), max: .init(width: 0, height: 300))
-let footerFrame = FrameConstant(min: .init(width: 0, height: 100), max: .init(width: 0, height: 200))
+let headerFrame = FrameConstant(
+    min: .init(width: 0, height: screenHeight * headerHeightRatio.0),
+    max: .init(width: 0, height: screenHeight * headerHeightRatio.1)
+)
+
+
+let centerFrame = FrameConstant(
+    min: .init(width: 0, height: screenHeight * centerHeightRatio.0),
+    max: .init(width: 0, height: screenHeight * centerHeightRatio.1)
+)
+
+
+let footerFrame = FrameConstant(
+    min: .init(width: 0, height: screenHeight * footerHeightRatio.0),
+    max: .init(width: 0, height: screenHeight * footerHeightRatio.1)
+)
+
