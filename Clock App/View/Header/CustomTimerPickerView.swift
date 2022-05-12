096c62338ec18975f13928e69280898445404435
seo-kh <gospelseokh@gmail.com>
2022-05-13 02:29:27 +0900

    Fix the Custom Picker View Layout: 3 components

--- Clock App/View/Header/CustomTimerPickerView.swift
+++ Clock App/View/Header/CustomTimerPickerView.swift
@@ -7,9 +7,12 @@
 
 import SwiftUI
 
-struct CustomTimerPickerView: View {
+struct CustomTimerPickerViewComponent: View {
     
     // MARK: - PROPERTIES
+    
+    var unit: String
+    let time: Range<Int>
     
     // MARK: - BODY
     
@@ -17,38 +20,49 @@ struct CustomTimerPickerView: View {
         ScrollView(.vertical, showsIndicators: false) {
             ScrollViewReader { value in
                 VStack(spacing: 0) {
-                    ForEach(0..<60) { second in
+                    ForEach(time, id: \.self) { second in
                         HStack {
                             GeometryReader { geo in
                                 Text("\(second)")
                                     .font(.title)
                                     .fontWeight(.semibold)
                                     .foregroundColor(.secondary)
-                                    .frame(width: 200, height: 50)
+                                    .frame(width: 50, height: 50)
                                     .id(second)
-                                    .rotation3DEffect(.degrees(Double(geo.frame(in: .global).maxY) / 2.5 - 170), axis: (x: 1.0, y: 0.0, z: 0.0), anchorZ: -5)
+                                    .rotation3DEffect(.degrees(Double(geo.frame(in: .global).maxY) / 2.5 - 200), axis: (x: 1.0, y: 0.0, z: 0.0), anchorZ: -2)
                                     
                             } //: GEOMETRY
-                            .frame(width: 200, height: 50)
+                            .frame(width: 50, height: 50)
                         } //: HSTACK
                     } //: LOOP
                 } //: VSTACK
-                .padding(.vertical, 175)
+                .padding(.vertical, 125)
             }
         } //: SCROLL
-        .frame(maxWidth: 300, maxHeight: 400)
+        .frame(maxWidth: 100, maxHeight: 300)
         .overlay(
-            Text("S")
+            Text(unit)
                 .font(.title2)
                 .fontWeight(.bold)
-                .offset(x: 30)
+                .offset(x: 32)
             , alignment: .center
         )
-        .overlay(
+    }
+}
+
+struct CustomTimerPickerView: View {
+    var body: some View {
+        HStack {
+            CustomTimerPickerViewComponent(unit: "H", time: 0..<12)
+            CustomTimerPickerViewComponent(unit: "M", time: 0..<60)
+            CustomTimerPickerViewComponent(unit: "S", time: 0..<60)
+        }
+        .padding()
+        .overlay(alignment: .center) {
             RoundedRectangle(cornerRadius: 10.0)
                 .fill(Color.white.opacity(0.2))
                 .frame(height: 50)
-        )
+        }
     }
 }
 
